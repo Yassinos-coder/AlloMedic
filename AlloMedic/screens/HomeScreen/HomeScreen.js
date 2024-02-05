b3bcf00e07ad7ccbf635781808220bae1b25b841
Yassinos <castroyassine0@gmail.com>
2024-02-05 16:13:30 +0100

    First Commit

---
+++ AlloMedic/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,14 @@
+import { HomeStyle } from './HomeStyles'
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View style={HomeStyle.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
---
+++ AlloMedic/screens/HomeScreen/HomeStyles.js
@@ -0,0 +1,6 @@
+import { StyleSheet } from "react-native";
+
+export const HomeStyle = StyleSheet.create({
+
+})
+
