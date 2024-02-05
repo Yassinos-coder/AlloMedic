18dbc562da2fca7b5ac2e671a1c417e1ff8033a1
Yassinos <castroyassine0@gmail.com>
2024-02-05 15:45:36 +0100

    Font loading bug

--- app/App.js
+++ app/App.js
@@ -1,8 +1,36 @@
+import React, { useState } from "react";
 import { Provider } from "react-redux";
 import Navigator from "./utils/Navigator";
 import Store from "./redux/Store";
+import * as Font from "expo-font";
+import { AppLoading } from "expo-app-loading";
+
+// Function to load the font
+const fetchFonts = () => {
+  return Font.loadAsync({
+    poppins: require("./assets/fonts/poppins.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  // Load the font before rendering the app
+  const loadFont = async () => {
+    await fetchFonts();
+    setFontLoaded(true);
+  };
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={loadFont}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <Provider store={Store}>
       <Navigator />
