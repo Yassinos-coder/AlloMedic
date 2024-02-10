dc6caef7759f24010fce68352202873319a7721d
Yassinos <castroyassine0@gmail.com>
2024-02-10 19:43:05 +0100

    Added Role Based SignUp UI

--- AlloMedic/screens/SignupScreen/SignupScreen.js
+++ AlloMedic/screens/SignupScreen/SignupScreen.js
@@ -1,12 +1,85 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, Pressable } from "react-native";
+import React, { useState } from "react";
 import { SignupStyles } from "./SignupStyles";
-
+import { AntDesign } from "@expo/vector-icons";
 
 const SignupScreen = () => {
+  const [role, setRole] = useState("user");
   return (
-    <View>
-      <Text>SignupScreen</Text>
+    <View style={SignupStyles.container}>
+      <View style={SignupStyles.headerSignup}>
+        <AntDesign
+          style={SignupStyles.BackArrow}
+          name="arrowleft"
+          size={25}
+          color="black"
+        />
+        <Image
+          style={SignupStyles.imageRightSide}
+          source={require("../../assets/images/SignupScreenRightImage.png")}
+        />
+      </View>
+      <Text
+        style={{
+          fontSize: 24,
+          color: "#333333",
+          fontFamily: "poppins",
+          marginLeft: 20,
+          marginTop: -80,
+        }}
+      >
+        Create New Account
+      </Text>
+      <View style={SignupStyles.roleView}>
+        <Pressable
+          onPress={() => {
+            if (role === "medic") {
+              setRole("user");
+            } else {
+              setRole("medic");
+            }
+          }}
+        >
+          <View
+            style={[
+              SignupStyles.userRole,
+              role === "medic" ? {} : { backgroundColor: "#333333" },
+            ]}
+          >
+            <Text
+              style={
+                role === "medic" ? { color: "#3A1E5D" } : { color: "white" }
+              }
+            >
+              User
+            </Text>
+          </View>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            if (role === "user") {
+              setRole("medic");
+            } else {
+              setRole("user");
+            }
+          }}
+        >
+          <View
+            style={[
+              SignupStyles.userRole,
+              role === "medic" ? { backgroundColor: "#333333" } : {},
+            ]}
+          >
+            <Text
+              style={
+                role === "medic" ? { color: "white" } : { color: "#3A1E5D" }
+              }
+            >
+              Medic
+            </Text>
+          </View>
+        </Pressable>
+      </View>
     </View>
   );
 };
--- AlloMedic/screens/SignupScreen/SignupStyles.js
+++ AlloMedic/screens/SignupScreen/SignupStyles.js
@@ -1,5 +1,42 @@
 import { StyleSheet } from "react-native";
 
 export const SignupStyles = StyleSheet.create({
-
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  headerSignup: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  BackArrow: {
+    backgroundColor: "#F4F4F4",
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    padding: 10,
+    margin: 10,
+  },
+  imageRightSide: {
+    width: 205,
+    height: 190.5,
+  },
+  roleView: {
+    marginTop: 15,
+    alignSelf: "center",
+    width: 343,
+    height: 50,
+    backgroundColor: "#F4F4F4",
+    borderRadius: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems:"center"
+  },
+  userRole: {
+    width: 164,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20
+  },
+});
