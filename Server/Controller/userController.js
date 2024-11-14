afd1c58cfd908966a8c90125c0a7acd09bcd7b9c
Yassinos <yassinecastro@proton.me>
2024-11-14 09:31:09 +0100

    Signup/in, user basic actions coded

--- Server/Controller/userController.js
+++ Server/Controller/userController.js
@@ -3,6 +3,7 @@ const UserModel = require('../Schemas/UserModel');
 const { tokenSigner, verifyToken } = require('../utils/jwt');
 const { EncryptData } = require('../utils/dataCrypter');
 const { SendEmail } = require('../utils/NodemailerConfig');
+const { sendTextMessage } = require('../utils/TwilioConfig');
 const saltRounds = 10;
 
 exports.signup = async (req, res) => {
@@ -92,6 +93,58 @@ exports.verifyEmail = async (req, res) => {
     }
 }
 
-exports.verifyPhone = async (req, res) => {
-    // to work when I buy the TWILIO UPGRADE
+exports.SendVerifyPhone = async (req, res) => {
+    try {
+        let _id = req.params.id
+        const user = await UserModel.findOne({ _id: _id })
+        const code = _id.slice(-6);
+        const message = `Here is your code to verify your phone number ${code}`
+        sendTextMessage(user.phonenumber, message)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+
+    }
 }
+
+exports.VerifyPhone = async (req, res) => {
+    try {
+        let _id = req.params.id
+        let VerificationCode = req.params.VerificationCode
+        const code = _id.slice(-6);
+        if (VerificationCode === code) {
+            await UserModel.updateOne({ _id: _id }, { is_verified_user: true })
+            res.status(200).json({ message: 'PHONENUMBER_VERIFIED' })
+        } else {
+            res.status(500).json({ message: 'PHONENUMBER_UNVERIFIED' })
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+exports.UpdatePassword = async (req, res) => {
+    try {
+        let newPasswordData = req.body;
+        const user = await UserModel.findOne({ _id: newPasswordData.uuid });
+        if (!user || !user.is_verified_user) {
+            return res.status(400).json({ message: 'ERROR_NOT_EXIST_OR_UNVERIFIED' });
+        }
+
+        const isOldPasswordCorrect = bcrypt.compareSync(newPasswordData.oldPassword, user.password);
+        if (!isOldPasswordCorrect) {
+            return res.status(400).json({ message: 'WRONG_OLD_PASSWORD' });
+        }
+
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const newHashedPassword = bcrypt.hashSync(newPasswordData.newPassword, salt);
+        const didUpdate = await UserModel.updateOne({ _id: newPasswordData.uuid }, { password: newHashedPassword });
+
+        if (didUpdate.modifiedCount > 0) {
+            return res.status(200).json({ message: 'UPDATE_SUCCESS' });
+        } else {
+            return res.status(500).json({ message: 'SOMETHING_WENT_WRONG' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+};
--- Server/Routes/Routes.js
+++ Server/Routes/Routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {verifyToken} = require('../utils/jwt')
 const {validateEmail, validateUser } = require('../Utils/userSanitizer');
-const { signup, signin, SendVerifyEmail, verifyEmail } = require('../Controller/userController');
+const { signup, signin, SendVerifyEmail, verifyEmail, SendVerifyPhone, VerifyPhone } = require('../Controller/userController');
 
 
 // User Routes to controllers
@@ -10,6 +10,8 @@ router.post('/signup', validateUser, signup);
 router.post('/signin', validateEmail, signin);
 router.post('/SendVerificationEmail/:id', SendVerifyEmail);
 router.post('/verifyEmail/:id/:VerificationCode', verifyEmail);
+router.post('/SendVerifyPhone/:id', SendVerifyPhone);
+router.post('/VerifyPhone/:id/:VerificationCode', VerifyPhone);
 
 
 module.exports = router;
